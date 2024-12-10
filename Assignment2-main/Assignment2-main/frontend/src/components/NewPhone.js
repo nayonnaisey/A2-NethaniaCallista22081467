bcd7280dae23f5f57d12b429de464f348de831b5
nayonnaisey <nethania.callista@gmail.com>
2024-12-10 18:47:12 +0700

    Task 1

--- Assignment2-main/Assignment2-main/frontend/src/components/NewPhone.js
+++ Assignment2-main/Assignment2-main/frontend/src/components/NewPhone.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 
 function NewPhone(props) {
-    const {contact, phones, setPhones} = props;
+    const { contact, phones, setPhones } = props;
     const [number, setNumber] = useState('');
     const [name, setName] = useState('');
 
@@ -29,13 +29,29 @@ function NewPhone(props) {
         setName('');
     }
 
-	return (
+    return (
         <form onSubmit={createPhone} onClick={(e) => e.stopPropagation()} className='new-phone'>
-            <input type='text' placeholder='Name' onChange={(e) => setName(e.target.value)} value={name}/>
-            <input type='text' placeholder='Phone Number' onChange={(e) => setNumber(e.target.value)} value={number}/>
-            <button className='button green' type='submit'>Add</button>
+            <select 
+                onChange={(e) => setName(e.target.value)} 
+                value={name} 
+                required
+            >
+                <option value="" disabled>Select Category</option>
+                <option value="Family">Family</option>
+                <option value="Friends">Friends</option>
+                <option value="Work">Work</option>
+                <option value="Others">Others</option>
+            </select>
+            <input 
+                type='text' 
+                placeholder='Phone Number' 
+                onChange={(e) => setNumber(e.target.value)} 
+                value={number} 
+                required
+            />
+            <button className='button green' type='submit'>Add Phone</button>
         </form>
-	);
+    );
 }
 
 export default NewPhone;
